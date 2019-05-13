6dcd7465288eab88ca9e71ff56210ab22c8d553c
Dieselmarble <lizhongxuanchina@gmail.com>
2019-05-13 18:29:50 +0100

    Add files via upload

---
+++ Arnold scrambling/arnold.m
@@ -0,0 +1,18 @@
+%img grey scale image; b coefficient; n transforming iterations
+function arnoldImg = arnold(img,a,b,n)
+[h,w] = size(img);
+N=h;
+arnoldImg = zeros(h,w);
+for i=1:n
+    for y=1:h
+        for x=1:w
+            %prevent errors in modulus, change axis from 0 to N-1
+            xx=mod((x-1)+b*(y-1),N)+1;
+            yy=mod(a*(x-1)+(a*b+1)*(y-1),N)+1;  
+            arnoldImg(yy,xx)=img(y,x);              
+        end
+    end
+    img=arnoldImg;
+end
+arnoldImg = uint8(arnoldImg);
+end
