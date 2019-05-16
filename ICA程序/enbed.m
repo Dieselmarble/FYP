b9e1ef8cac9faa30704d6faaf0b91376c194f0d9
Dieselmarble <lizhongxuanchina@gmail.com>
2019-05-16 17:15:26 +0100

    Update enbed.m

--- ICA程序/enbed.m
+++ ICA程序/enbed.m
@@ -1,34 +1,43 @@
 clear all; close all;
-A = imread('lena.jpg');
-% A=OpenBitmap('6.tif');
+A = imread('lena.jpg'); %A=OpenBitmap('6.tif');
 k=64;
-l=64; %32
+l=64;
 S_CI=DvdBptSubBp(A,k,l); %split into patches
+% normalise
+%S_CI = normalize(S_CI);
+% whitening
+%S_CI = whiten(S_CI);
+%-----------------------------------------------------------------
 [icasig, A_matrix, W_matrix] =fastica(S_CI); %independent signals; mixing matrix; sepration matrix
 % A_matrix is 'ICA coefficients' which has super guassian distribution
+% icasig is the basis funcions
+% show all ica feature basis
+%icasig(49,:)=1;
+%A_matrix(:,49)=1;
+%W_matrix(49,:)=1;
+figure;
+for i = 1:49
+    subplot(7,7,i);
+    imshow(reshape(icasig(i,:),64,64));
+end
 %---Step1------------------------------------------------------------------
-mark = imread('changsha.bmp');
+mark = imread('changsha.bmp')*255;
+%mark = imresize(mark, 0.5);
 W=WMToV(mark); %wartermark.bmp
 %--------------------------------------------------------------------------
 index=max_cov(icasig); %find maximum covariance(energy) in decomposed signal
 S_W=[icasig(index,:); W];
-SS = [0.5 0.5; 0.1 0.9];
+SS = [0.8 0.1; 0.9 0.2];
 %--------------------------------------------------------------------------
 
 %-----Step3:Y=A*S_W--------------------------------------------------------
-Y=SS*S_W; %Y is a 2X2 matrix, linear combination of two observations
+Y=SS*double(S_W); %Y is a 2X2 matrix, linear combination of two observations
 y1=Y(1,:);
 y2=Y(2,:); 
 %--------------------------------------------------------------------------
-icasig(index,:)=y1; %embedded watermark in max covariance
+icasig(index,:)=y1;
 S_CI=inv(W_matrix)*icasig; % Remix the signals
 SubBPtBP(A,k,l,S_CI); % Restore watermarked image
 %--------------------------------------------------------------------------
-figure(1);
+figure;
 imshow(A);
-
-
-
-
-
-
