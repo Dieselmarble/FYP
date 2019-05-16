d382ed10b1dff988dc6b8cf4997a88ae900d6a7c
Dieselmarble <lizhongxuanchina@gmail.com>
2019-05-16 17:35:10 +0100

    Update enbed.m

--- ICA程序/enbed.m
+++ ICA程序/enbed.m
@@ -4,32 +4,32 @@ k=64;
 l=64;
 S_CI=DvdBptSubBp(A,k,l); %split into patches
 % normalise
-%S_CI = normalize(S_CI);
+% S_CI = normalize(S_CI);
 % whitening
 %S_CI = whiten(S_CI);
 %-----------------------------------------------------------------
 [icasig, A_matrix, W_matrix] =fastica(S_CI); %independent signals; mixing matrix; sepration matrix
+figure;
+histogram(A_matrix);
 % A_matrix is 'ICA coefficients' which has super guassian distribution
 % icasig is the basis funcions
 % show all ica feature basis
-%icasig(49,:)=1;
-%A_matrix(:,49)=1;
-%W_matrix(49,:)=1;
-figure;
+% icasig(49,:)=0.001;
+% A_matrix(:,49)=0.001;
+% W_matrix(49,:)=0.001;
+% figure;
 for i = 1:49
     subplot(7,7,i);
     imshow(reshape(icasig(i,:),64,64));
 end
 %---Step1------------------------------------------------------------------
-mark = imread('changsha.bmp')*255;
+mark = imread('changsha.bmp');
 %mark = imresize(mark, 0.5);
 W=WMToV(mark); %wartermark.bmp
 %--------------------------------------------------------------------------
 index=max_cov(icasig); %find maximum covariance(energy) in decomposed signal
 S_W=[icasig(index,:); W];
 SS = [0.8 0.1; 0.9 0.2];
-%--------------------------------------------------------------------------
-
 %-----Step3:Y=A*S_W--------------------------------------------------------
 Y=SS*double(S_W); %Y is a 2X2 matrix, linear combination of two observations
 y1=Y(1,:);
