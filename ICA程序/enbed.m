072cfefca86aebdef39e51fed7d0944be89e56bd
Dieselmarble <lizhongxuanchina@gmail.com>
2019-05-16 17:38:37 +0100

    Update enbed.m

--- ICA程序/enbed.m
+++ ICA程序/enbed.m
@@ -6,7 +6,7 @@ S_CI=DvdBptSubBp(A,k,l); %split into patches
 % normalise
 % S_CI = normalize(S_CI);
 % whitening
-%S_CI = whiten(S_CI);
+% S_CI = whiten(S_CI);
 %-----------------------------------------------------------------
 [icasig, A_matrix, W_matrix] =fastica(S_CI); %independent signals; mixing matrix; sepration matrix
 figure;
@@ -17,14 +17,13 @@ histogram(A_matrix);
 % icasig(49,:)=0.001;
 % A_matrix(:,49)=0.001;
 % W_matrix(49,:)=0.001;
-% figure;
+figure;
 for i = 1:49
     subplot(7,7,i);
     imshow(reshape(icasig(i,:),64,64));
 end
 %---Step1------------------------------------------------------------------
 mark = imread('changsha.bmp');
-%mark = imresize(mark, 0.5);
 W=WMToV(mark); %wartermark.bmp
 %--------------------------------------------------------------------------
 index=max_cov(icasig); %find maximum covariance(energy) in decomposed signal
