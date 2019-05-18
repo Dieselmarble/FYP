c3556dc7a47bd5504fd94cd382b572fb63216f9c
Dieselmarble <lizhongxuanchina@gmail.com>
2019-05-18 18:25:11 +0100

    Add files via upload

--- ICA程序/enbed.m
+++ ICA程序/enbed.m
@@ -1,5 +1,6 @@
 clear all; close all;
-A = imread('lena.jpg'); %A=OpenBitmap('6.tif');
+A = imread('lena.jpg'); 
+% A=OpenBitmap('6.tif');
 k=64;
 l=64;
 S_CI=DvdBptSubBp(A,k,l); %split into patches
@@ -9,6 +10,26 @@ S_CI=DvdBptSubBp(A,k,l); %split into patches
 % S_CI = whiten(S_CI);
 %-----------------------------------------------------------------
 [icasig, A_matrix, W_matrix] =fastica(S_CI); %independent signals; mixing matrix; sepration matrix
+% -----frequency spectrum ------
+% figure;
+% for i = 1:49
+%     Fs = 2000;
+%     t = 0:1/Fs:1-1/Fs;
+%     N = length(icasig(i,:));
+%     xdft = fft(icasig(i,:));
+%     xdft = xdft(1:N/2+1);
+%     psdx = (1/(Fs*N)) * abs(xdft).^2;
+%     psdx(2:end-1) = 2*psdx(2:end-1);
+%     freq = 0:Fs/length(icasig(i,:)):Fs/2;
+%     hold on
+%     plot(freq,10*log10(psdx));
+% end
+% hold off
+% grid on
+% title('Periodogram Using FFT')
+% xlabel('Frequency (Hz)')
+% ylabel('Power/Frequency (dB/Hz)')
+% ------- histogram ---------------
 figure;
 histogram(A_matrix);
 % A_matrix is 'ICA coefficients' which has super guassian distribution
@@ -17,18 +38,21 @@ histogram(A_matrix);
 % icasig(49,:)=0.001;
 % A_matrix(:,49)=0.001;
 % W_matrix(49,:)=0.001;
-figure;
-for i = 1:49
-    subplot(7,7,i);
-    imshow(reshape(icasig(i,:),64,64));
-end
+% figure;
+% for i = 1:49
+%     subplot(7,7,i);
+%     imshow(reshape(icasig(i,:),64,64));
+% end
 %---Step1------------------------------------------------------------------
 mark = imread('changsha.bmp');
+b_CI=DvdBptSubBp(mark,k,l);
+[ica_mark, Ab_matrix, Wb_matrix] =fastica(b_CI);
 W=WMToV(mark); %wartermark.bmp
+
 %--------------------------------------------------------------------------
 index=max_cov(icasig); %find maximum covariance(energy) in decomposed signal
 S_W=[icasig(index,:); W];
-SS = [0.8 0.1; 0.9 0.2];
+SS = [0.5 0.5; 0.5 0.5];
 %-----Step3:Y=A*S_W--------------------------------------------------------
 Y=SS*double(S_W); %Y is a 2X2 matrix, linear combination of two observations
 y1=Y(1,:);
--- ICA程序/extrack.m
+++ ICA程序/extrack.m
@@ -1,7 +1,7 @@
 close all;
-% add attacks
+% -------- add attacks --------------
 % ------- gaussian noise attack ----------
-%A_ = imnoise(A,'gaussian',0,0.01);
+A_ = imnoise(A,'gaussian',0,0.01);
 % ------- cropping attack ---------
 % A_ = imcrop(A,[0 0 240 240]);
 % A_ = padarray(A_,[240 240],0,'post');
@@ -9,7 +9,8 @@ close all;
 % A_ = fspecial('gaussian',3,1);
 % attackf=filter2(h,f);
 % ------- salt & pepper noise -------
-A_ = imnoise(A,'salt & pepper',0.0002);
+% A_ = A;
+% A_ = imnoise(A,'salt & pepper',0.0002);
 % reshape the image into patches
 S_CI_=DvdBptSubBp(A_,k,l);
 %--------------------------------------------------------------------------
@@ -19,11 +20,11 @@ Y_=[icasig_(index_,:); y2];
 %--------------------------------------------------------------------------
 [Wartermark] = fastica(Y_);
 Wartermark = abs(Wartermark);
-%Wartermark = normalize(Wartermark);
+% Wartermark = normalize(Wartermark);
 WWW=ones(k,l);             
 if mean(Wartermark(2,:))<mean(Wartermark(1,:))
     mmm=Wartermark(2,:);
-else mmm=Wartermark(1,:); %choose the minimum row
+else mmm=Wartermark(1,:); %choose the minimum row which contains the watermark
 end
 %--------------------------------------------------------------------------
 for i=1:l
