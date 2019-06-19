ce59fe8885a22f88ca8cddd7ea4f299a9f49c195
Dieselmarble <lizhongxuanchina@gmail.com>
2019-06-19 23:33:29 +0100

    reupload all codes

--- Block_sparse/Optisens.m
+++
@@ -1,8 +0,0 @@
-function A = Optisens(D, M)
-%Sensing matrix design for sparse coding
-
-[N K] = size(D);
-[U,S,V] = pca(D*D',M);
-A = sqrt(inv(S))*U';
-
-
--- Block_sparse/block_sparsity.m
+++
@@ -1,7 +0,0 @@
-function b = block_sparsity(X, d, th)
-
-b = 0; L = size(X,2);
-for i = 1:max(d)
-    b = b+sum(sum((X(d==i,:)).^2)>th);
-end
-b = b/L;
